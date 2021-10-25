c3eb5900dc9c6e7c0e5f11b7fcd8e5478679d6bc
edem <edem@neattech.io>
2021-10-25 21:30:48 +0300

    test

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { port } = require("./configuration");
-const { connectDb } = require("./helpers/db");
+const { connectDb } = require("./helpers/db")
 
 const app = express();
 const postSchema = new mongoose.Schema({

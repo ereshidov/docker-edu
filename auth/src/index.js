bd7b6fbc49244653ab89ab2987cc675960fd335c
edem <edem@neattech.io>
2021-10-25 21:59:18 +0300

    setup auth server
    add: frontend part

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const { port } = require("./configuration");
 const { connectDb } = require("./helpers/db");
 
 const app = express();
-
-const Post = mongoose.model('Post', postSchema);
 
 const startServer = () => {
   app.listen(port, () => {

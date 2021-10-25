5fc1f607316a342e6932347bd24d3762b184eca4
edem <edem@neattech.io>
2021-10-25 21:34:47 +0300

    fix: auth index file

--- auth/src/index.js
+++ auth/src/index.js
@@ -4,23 +4,17 @@ const { port } = require("./configuration");
 const { connectDb } = require("./helpers/db");
 
 const app = express();
-const postSchema = new mongoose.Schema({
-  name: String
-});
 
 const Post = mongoose.model('Post', postSchema);
 
 const startServer = () => {
   app.listen(port, () => {
-    console.log(`API service was started, on port: ${port}`);
-
-    const testPost = new Post({name: 'Silence'});
-
+    console.log(`AUTH service was started, on port: ${port}`);
   });
 };
 
 app.get("/test", (req, res) => {
-  res.send("API server is working correctly");
+  res.send("AUTH server is working correctly");
 });
 
 connectDb()

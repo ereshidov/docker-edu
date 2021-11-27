24d2fc58b90e079099af323af99bcafb8e01f707
edem <edem@neattech.io>
2021-11-27 18:11:58 +0300

    add: requests between services

--- api/src/index.js
+++ api/src/index.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { port } = require("./configuration");
+const axios = require('axios');
+const { port, authApiUrl } = require("./configuration");
 const { connectDb } = require("./helpers/db");
 
 const app = express();
 const postSchema = new mongoose.Schema({
-  name: String
+  name: String,
 });
 
-const Post = mongoose.model('Post', postSchema);
+const Post = mongoose.model("Post", postSchema);
 
 const startServer = () => {
   app.listen(port, () => {
     console.log(`API service was started, on port: ${port}`);
 
-    const testPost = new Post({name: 'Silence'});
-
+    const testPost = new Post({ name: "Silence" });
   });
 };
 
 app.get("/test", (req, res) => {
   res.send("API server is working correctly");
+});
+
+app.get("/testwithcurrentuser", (req, res) => {
+  axios.get(authApiUrl + '/currentUser').then((response => {
+    res.json({
+      testwithcurrentuser: true,
+      currentUserFromAuth: response.data,
+    })
+  }))
 });
 
 connectDb()
